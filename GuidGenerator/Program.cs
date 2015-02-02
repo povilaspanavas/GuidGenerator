a4b35be701a85bba7f94eb80d67b3b2d255ff905
Povilas Panavas <povilas.panavas@gmail.com>
2015-02-02 17:25:26 +0000

    Initial version for generating guids. Usage simple. Just paste the new guid

---
+++ GuidGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GuidGenerator
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 0:
+                    Clipboard.SetText(Guid.NewGuid().ToString());
+                    break;
+                case 1:
+                    var count = int.Parse(args[0]);
+                    var result = new List<string>();
+                    for (int i = 0; i < count; i++)
+                        result.Add(Guid.NewGuid().ToString());
+                    Clipboard.SetText(string.Join<string>(Environment.NewLine, result));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
